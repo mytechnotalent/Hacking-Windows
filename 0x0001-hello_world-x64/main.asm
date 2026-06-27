b72364ba6075777ab37d8c220f38309e803425a3
Solution <egorshugaev6@gmail.com>
2026-06-27 03:31:55 +0300

    Fix x64 MessageBox uType and stack reservation

--- 0x0001-hello_world-x64/main.asm
+++ 0x0001-hello_world-x64/main.asm
@@ -7,15 +7,15 @@ extrn PostQuitMessage: proc
 
 .code
 	main proc
-		sub	rsp, 20h			;shadow stack
+		sub	rsp, 28h			;shadow stack
 
-		mov	r9, rax				;UINT uType
+		xor	r9d, r9d				;UINT uType
 		lea	r8, msg_caption			;LPCSTR	lpCaption
 		lea	rdx, msg_txt			;LPCSTR	lpText
 		xor	rcx, rcx			;HWND hWnd
 		call MessageBoxA			;https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messageboxa
 		
-		add rsp, 20h				;restore shadow stack
+		add rsp, 28h				;restore shadow stack
 
 		xor	rcx, rcx			;int nExitCode
 		call PostQuitMessage			;https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-postquitmessage
